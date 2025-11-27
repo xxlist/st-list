3a57cc04ed0c7f0cc0838be49837b7bf9368515b
lost <lost>
2025-11-27 21:49:20 +0800

    opt

--- bin/main.ml
+++ bin/main.ml
@@ -5,15 +5,13 @@ open Cohttp_lwt
 open Cohttp_lwt_unix
 
 let with_out_file path f =
-    let out_c = open_out path in
-    Fun.protect ~finally:(fun () -> close_out out_c)
-    (fun () -> f out_c)
+  let out_c = open_out path in
+  Fun.protect ~finally:(fun () -> close_out out_c) (fun () -> f out_c)
 
 let read_lines path =
-    Lwt_io.with_file ~mode:Lwt_io.Input path (fun in_c ->
-      Lwt_io.read_lines in_c |> Lwt_stream.to_list
-    )
-  
+  Lwt_io.with_file ~mode:Lwt_io.Input path (fun in_c ->
+      Lwt_io.read_lines in_c |> Lwt_stream.to_list)
+
 (* let retry ?(limit=1) f =  *)
 (*   let rec aux count =  *)
 (*     Lwt.catch *)
@@ -30,23 +28,22 @@ let read_lines path =
 (*   in *)
 (*   aux 0 *)
 
-let retry_result ?(limit=1) f =
+let retry_result ?(limit = 1) f =
+  assert (limit >= 0);
   let rec aux count =
-    f() >>= function 
+    f () >>= function
     | Ok v -> Lwt.return_ok v
-    | Error ex -> 
-      if count = limit then 
-        Lwt.return_error ex
-      else 
-        let jitter_ms = Random.int 2000 in
-        let sleep_ms = 200 + jitter_ms in
-        let sleep_s = (float_of_int sleep_ms) /. 1000. in
-        Logs.warn (fun m -> m "Retry %d after %dms" count sleep_ms);
-        Lwt_unix.sleep sleep_s 
-        >>= fun () -> aux (count+1)
+    | Error ex ->
+        if count = limit then Lwt.return_error ex
+        else
+          let jitter_ms = Random.int 2000 in
+          let sleep_ms = 200 + jitter_ms in
+          let sleep_s = float_of_int sleep_ms /. 1000. in
+          Logs.warn (fun m -> m "Retry #%d after %dms" (count + 1) sleep_ms);
+          Lwt_unix.sleep sleep_s >>= fun () -> aux (count + 1)
   in
   aux 0
-  
+
 type info = {
   modelId : int;
   username : string;
@@ -62,62 +59,55 @@ type info = {
   isNonNude : bool;
   isMicInactive : bool;
   isBadStream : bool;
-} [@@deriving yojson {strict=false}]
+}
+[@@deriving yojson { strict = false }]
 
+type response = { item : info } [@@deriving yojson { strict = false }]
 
-type response = {
-  item : info;
-} [@@deriving yojson {strict=false}]
+type ext_inf = { title : string; logo_link : string; play_link : string }
+[@@deriving yojson { strict = false }]
 
-type ext_inf = {
-  title : string;
-  logo_link : string;
-  play_link : string;
-} [@@deriving yojson {strict=false}]
+type ext_m3u = { ext_inf_list : ext_inf list }
+[@@deriving yojson { strict = false }]
 
-type ext_m3u = {
-  ext_inf_list : ext_inf list;
-} [@@deriving yojson {strict=false}]
+let ext_inf_of_info info =
+  let model_id = string_of_int info.modelId in
+  let logo_link =
+    "https://adultiptv.net/livecams/image.php?id=sc_" ^ model_id
+  in
+  let play_link =
+    "https://adultiptv.net/livecams/stream.php?id=sc_" ^ model_id
+  in
+  { title = info.username; logo_link; play_link }
 
-let ext_inf_of_info info = 
-  let model_id = (string_of_int info.modelId) in
-  let logo_link  =  ("https://adultiptv.net/livecams/image.php?id=sc_" ^ model_id) in
-  let play_link = ("https://adultiptv.net/livecams/stream.php?id=sc_" ^ model_id) in
-  { title=info.username; logo_link=logo_link;play_link=play_link }
-
-let ext_m3u_of_info_list info_list = 
+let ext_m3u_of_info_list info_list =
   let ext_inf_list = List.map ext_inf_of_info info_list in
-  {ext_inf_list = ext_inf_list }
+  { ext_inf_list }
 
-let print_ext_inf fmt ei = 
+let print_ext_inf fmt ei =
   Format.pp_print_string fmt "#EXTINF:-1 ";
   Format.fprintf fmt "tvg-logo=%S,%s\n" ei.logo_link ei.title;
   Format.pp_print_string fmt ei.play_link;
   Format.pp_print_newline fmt ();
   Format.pp_print_flush fmt ()
-  
-let print_ext_m3u fmt em = 
+
+let print_ext_m3u fmt em =
   Format.pp_print_string fmt "#EXTM3U\n";
-  em.ext_inf_list 
-  |> List.iter (print_ext_inf fmt)
- 
-let decode_response json_str = 
-  try
-    json_str 
-    |> Yojson.Safe.from_string 
-    |> response_of_yojson 
+  em.ext_inf_list |> List.iter (print_ext_inf fmt)
+
+let decode_response json_str =
+  try json_str |> Yojson.Safe.from_string |> response_of_yojson
   with exn -> Error (Printexc.to_string exn)
 
 let fetch_response name =
-  let link = ("https://zh.stripchat.com/api/front/v1/broadcasts/" ^ name) in
+  let link = "https://zh.stripchat.com/api/front/v1/broadcasts/" ^ name in
   let url = Uri.of_string link in
   Client.get url >>= fun (resp, body) ->
-  let code = resp 
-    |> Response.status 
-    |> Code.code_of_status in
+  let code = resp |> Response.status |> Code.code_of_status in
   (* Logs.debug (fun m -> m "Http Code: %d" code); *)
-  if code <> 200 then 
-    Lwt.return_error (sprintf "Error fetch response: name=%S http-status-code=%d" name code) 
+  if code <> 200 then
+    Lwt.return_error
+      (sprintf "Error fetch response: name=%S http-status-code=%d" name code)
   else
     (* header *)
     (* let header = resp |> Response.headers in *)
@@ -127,44 +117,47 @@ let fetch_response name =
     (* Logs.debug (fun m ->  m "Http Body length: %d" (String.length data)); *)
     Lwt.return_ok data
 
-let fetch_info name = 
-  Logs.info (fun m -> m "Fetch info: name=%S" name);
+let fetch_info name =
+  Logs.info (fun m -> m "Fetching %S" name);
   fetch_response name >>= function
-  | Ok str -> decode_response str |> Result.map (fun res -> res.item) |> Lwt.return
+  | Ok str ->
+      decode_response str |> Result.map (fun res -> res.item) |> Lwt.return
   | Error ex -> Error ex |> Lwt.return
 
 let fetch_info_wtih_retry name =
-  retry_result ~limit:3 (fun () -> fetch_info name)
-  
-let fetch_info_list name_list = 
-  name_list 
-  |> List.map fetch_info_wtih_retry
-  |> Lwt.all
+  retry_result ~limit:3 (fun () -> fetch_info name) >>= function
+  | Ok v ->
+      Logs.info (fun m -> m "Fetched %S" name);
+      Lwt.return_ok v
+  | Error ex ->
+      Logs.err (fun m -> m "Failed to fetch %S" name);
+      Lwt.return_error ex
 
-let run () = 
-  read_lines "./st.txt" 
-  >>= fun name_list -> fetch_info_list name_list 
-  >>= fun result_list ->
+let fetch_info_list name_list =
+  name_list |> List.map fetch_info_wtih_retry |> Lwt.all
+
+let run () =
+  read_lines "./st.txt" >>= fun name_list ->
+  fetch_info_list name_list >>= fun result_list ->
   let info_list = result_list |> List.filter_map Result.to_option in
   ext_m3u_of_info_list info_list |> Lwt.return
 
 let save_ext_m3u file em =
   with_out_file file (fun out_c ->
-    let fmt = Format.formatter_of_out_channel out_c in
-    print_ext_m3u fmt em;
-  )
-    
-let setup_logs () = 
+      let fmt = Format.formatter_of_out_channel out_c in
+      print_ext_m3u fmt em)
+
+let setup_logs () =
   Logs.set_level (Some Logs.Info);
   Logs.set_reporter (Logs_fmt.reporter ())
-  
+
 (* === main === *)
 
-let () = 
+let () =
   setup_logs ();
   Logs.info (fun m -> m "=== Begin ===");
   let run = run () in
-  let em =  Lwt_main.run run in
+  let em = Lwt_main.run run in
   Logs.info (fun m -> m "%s" (em |> ext_m3u_to_yojson |> Yojson.Safe.to_string));
   Logs.info (fun m -> m "save st.m3u");
   save_ext_m3u "./st.m3u" em;
